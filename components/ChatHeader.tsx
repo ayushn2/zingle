bf9ae9d0a28e392a14d5822ec672117256b9eb35
Ayush Nainwal <aayu@Ayushs-MacBook-Air.local>
2024-03-16 04:43:47 +0530

    Enabled presence in the app

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -4,6 +4,7 @@ import {User} from "@supabase/supabase-js"
 import SignOut from './SignOut'
 import readUserSession from '@/lib/actions'
 import SignIn from './SignIn'
+import ChatPresence from './ChatPresence'
 
 const ChatHeader = async () => {
 
@@ -15,12 +16,7 @@ const ChatHeader = async () => {
 
       <div>
         <h1 className="text-xl font-bold">Zingle</h1>
-        <div className="flex item-center gap-1">
-          <div className="h-4 w-4 bg-green-500 rounded-full animate-pulse">
-
-          </div>
-          <h1 className="text-sm text-gray-400">2 online</h1>
-        </div>
+       <ChatPresence/>
       </div>
       {/* {user?(
       <Button onClick={handleLoginWithGithub}>
